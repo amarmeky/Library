829f1142364f4fb9e872794448d96f0103a7d1cd
Amar Meky <amarmeky7@gmail.com>
2025-11-09 19:03:55 +0200

    add table database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[CategoryController::class, 'all']
+);
